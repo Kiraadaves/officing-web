b4064818629fce8e7d7fad61792c2ecb9cac9ceb
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2024-03-05 15:16:13 +0100

    progress

---
+++ app/Assets/components/new.tsx
@@ -0,0 +1,17 @@
+
+
+const New = () => {
+  return (
+    <div>
+      <div className="bg-[#fff] my-10 py-5 rounded-lg flex flex-col gap-3 items-center">
+
+        <h1 className="text-[#090A13] font-bold text-2xl tracking-wide">Keeping track of your inventory has never been easier</h1>
+        
+        <p className="text-lg">Create a list of products and services. To make your invoicing faster and cleaner</p>
+        
+      </div>
+    </div>
+  )
+}
+
+export default New;
