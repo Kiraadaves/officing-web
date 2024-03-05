efc3ecf384926e4b2346d0269fe85feb8c2c012b
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2024-03-05 15:16:13 +0100

    progress

--- app/Assets/index.tsx
+++ app/Assets/index.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header'
+import New from './components/new';
 
 const Assets = () => {
   return (
     <div>
       <Header pageTitle="Assets" />
+      <New />
     </div>
   )
 }
