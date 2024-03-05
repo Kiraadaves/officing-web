af222946aff1feb966186a07105fd1ce86f37e45
Akinuliola Akinbobola Oluwafemi <71355010+phemmyblaze@users.noreply.github.com>
2024-03-06 00:55:38 +0100

    in progress

---
+++ app/Assets/addNew/index.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+
+const Addnew = () => {
+  return (
+    <>
+        <Header pageTitle="New Items" />
+        hello
+      
+     
+    </>
+  )
+}
+
+export default Addnew;
--- app/Assets/index.tsx
+++ app/Assets/index.tsx
@@ -1,10 +1,9 @@
-import Header from '@/components/Header'
 import New from './components/new';
 
 const Assets = () => {
   return (
     <div>
-      <Header pageTitle="Assets" />
+      
       <New />
     </div>
   )
